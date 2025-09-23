7943b27764b7d4a53d9d34e41ee0b5f7f1192d32
2004Snehal <66834515+2004Snehal@users.noreply.github.com>
2025-09-23 21:48:32 +0530

    profile photo changed

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowDown, Github, Linkedin, Mail } from "lucide-react";
-import profileImage from "@assets/Screenshot 2025-09-16 184959_1758043574666.png";
+import profileImage from "@assets/snehal_photo.jpg";
 
 export default function HeroSection() {
   const scrollToSection = (sectionId: string) => {
